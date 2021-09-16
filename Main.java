ff90f528db4830d422f0d4e499f30cebeb961c2f
Rafael <rafael.resende@acad.pucrs.br>
2021-09-15 21:04:10 -0300

    fixes

--- Main.java
+++ Main.java
@@ -2,12 +2,19 @@ import java.util.ArrayList;
 import java.nio.file.*;
 import java.io.*;
 
+//TODO -> Conseguir fazer mais de uma run com seeds diferentes
+//TODO -> Zerar as listas depois de cada run
+//TODO -> Armazenar o tempo de cada execução e depois fazer a média
+//TODO -> Corrigir e retirar detalhes irrelevantes do código
+//TODO -> Armazenar o tempo que a fila ficou em cada estado/capacidade
+//TODO -> Calcular a probabilidade que a fila ficou em cada estado
+
 public class Main{
     public static void main(String[] args) {
 
         //Definição de valores do Gerador Aleatório
-        int quantSeeds = 3; // Selecione a quantidade de seeds e também quantas vezes o programa irá rodar
-        int perSeed = 2000; // número de aleatórios por seed
+        int quantSeeds = 5; // Selecione a quantidade de seeds e também quantas vezes o programa irá rodar (>=1)
+        int perSeed = 100; // Número de aleatórios por seed
         double a = 1644525;
         double M = Math.pow(2, 32);
         double c = 1013904223;
@@ -49,23 +56,37 @@ public class Main{
         parametros.add(3);         // 7atendimento (
         parametros.add(5);        // 8atendimento )
         parametros.add(perSeed); // 9perSeed
+        parametros.add(quantSeeds); // 10
+        parametros.add(a);      // 11
+        parametros.add(M); //12
+        parametros.add(c); //13
 
         //Começa a simulação
+        int run = 0;
+
         while (quantSeeds > 0){
 
+            run = run + 1;
+
             //define a seed inicial para essa run
-            int and = (int) seeds.get(quantSeeds);
+            int and = (int) seeds.get(quantSeeds - 1);
             
             //rodar a lista 
             ListaFinal = PseudoGenerator(perSeed, a, M, c, ListaFinal, and);
+
+            System.out.println("\nINFORMACOES RELEVANTES:\n");
+
+            System.out.println("Run: " + run + "\n");
+            System.out.println("Utilizando seed: " + and + "\n");
+            System.out.println("Primeira Chegada no tempo: " + 3 + " \n");
+            System.out.println("Fila com chegadas entre: " + parametros.get(5) + "..." + parametros.get(6) + "\n");
+            System.out.println("Fila com atendimento entre: " + parametros.get(7) + "..." + parametros.get(8) + "\n");
+            System.out.println("G/G/" + parametros.get(2) + "/" + parametros.get(3));
 
             interpreter(eventoInicial, ListaFinal, parametros, tempos, escalonador, eventos, eventosNome);
 
             //fazer um metodo pra zerar as listas e rodar dnv #TODO
             quantSeeds -= 1;
-
-            System.out.println("rodou " + quantSeeds );
-
         }
     }
 
@@ -77,11 +98,15 @@ public class Main{
         int servidores = (int) p.get(2);
         int capacidade = (int) p.get(3);
         int perda = (int) p.get(4);
+        int chA = (int) p.get(5);
+        int chB = (int) p.get(6);
+        int atA = (int) p.get(7);
+        int atB = (int) p.get(8);
         int perSeed = (int) p.get(9);
-        int chA = 2;
-        int chB = 4;
-        int atA = 3;
-        int atB = 5;
+        double a = (Double) p.get(11);
+        double M = (Double) p.get(12);
+        double c = (Double) p.get(13);
+
         double tempo = 0;
 
         if (eventos < perSeed){
@@ -98,10 +123,10 @@ public class Main{
                     e.add(tempo);
                     evenNm.add(evento);
 
-                    even.add(0); // gastou um aleatório
+                    even.add(0); // gasta um aleatório
                     l.remove(0);
 
-                    System.out.println("\n Chegada agendada -> " + " " + tempo);
+                    System.out.println(" Chegada agendada <<<< " + " " + tempo + " \n");
                 } break;
 
                 case "SAIDA":{
@@ -112,13 +137,15 @@ public class Main{
                     e.add(tempo);
                     evenNm.add(evento);
 
-                    even.add(0); // gastou um aleatório
+                    even.add(0); // gasta um aleatório
                     l.remove(0);
 
-                    System.out.println("\n Saida agendada ->" + tempo + " \n");
+                    System.out.println(" Saida agendada >>>> " + tempo + " \n");
                 } break;
 
                 case "CHEGADAINICIAL":{
+
+                    System.out.println("\n<><><><>   INICIANDO SIMULACAO  <><><><> \n" );
 
                     double sorteio = 0;
                     tempo = tempoaux + sorteio;
@@ -126,11 +153,10 @@ public class Main{
                     e.add(3.0); //primeiro tempo
                     evenNm.add("CHEGADA");
 
-                    System.out.println("\n Primeira Chegada -> " + 3 + " \n");
+                    System.out.println("\n<<<< Chegada Inicial Agendada <<<<\n" );
 
                     escalonador(0, l, p, t, e, even, evenNm);
 
-                    interpreter("SOLICITA", l, p, t, e, even, evenNm);
                 } break;
 
                 case "SOLICITA":{
@@ -149,15 +175,15 @@ public class Main{
                         i++;
                         }
                     }
-
-                    System.out.println("O menor é: " + menor.doubleValue() + " na posição: " + pos);
+                    System.out.println("PROXIMO EVENTO: " + "\n");
+                    System.out.println("Escalonador" + "[" + pos + "] " + evenNm.get(pos) + " --- " + menor.doubleValue() + "\n");
 
                     escalonador(pos, l, p, t, e, even, evenNm);
                 } break;
             } 
             } else {
                 
-                 l = PseudoGenerator(perSeed, a, M, c, ListaFinal, and);
+                 l = PseudoGenerator(perSeed, a, M, c, l, (int) Math.round(((Double)l.get(0))));
 
             }
         }
@@ -176,29 +202,32 @@ public class Main{
 
                 case "CHEGADA":{
 
-                    System.out.println("\n Chegada Realizada");
+                    System.out.println("<<<< Chegada Realizada <<<<\n");
 
                     if ( fila < capacidade ){
                           fila++;
                           p.set(0, fila);
                         if( fila <= (servidores)){
+                            System.out.println(">>>> Agendando Saida >>>> \n");
                              interpreter("SAIDA", l, p, t, e, even, evenNm);
                              }
                         } else {
                             perda++;
                             p.set(4, perda);
                         }
+                        System.out.println("<<<< Agendando Chegada <<<< \n");
 
                         interpreter("CHEGADA", l, p, t, e, even, evenNm);
                 } break;
 
                 case "SAIDA":{
 
-                    System.out.println("\n Saida Realizada");
+                    System.out.println("\n >>>> Saida Realizada >>>> \n");
 
                     fila--;
                     p.set(0, fila);
                     if (fila >= servidores){
+                        System.out.println(">>>> Agendando Saida >>>> \n");
                         interpreter("SAIDA", l, p, t, e, even, evenNm);
                     }
                 } break;
@@ -208,9 +237,18 @@ public class Main{
                 p.set(1, tempo + soma);
                 e.remove(pos);
                 evenNm.remove(pos);
-                System.out.println(e);
-                System.out.println(evenNm);
-                System.out.println(p);
+
+                //Prints
+                System.out.println("ESTADO DO ESCALONADOR: \n");
+
+                for (int i = 0; i < e.size(); i++){
+                    System.out.println(" Escalonador" + "[" + i + "]" + "----> " + evenNm.get(i) + " --------- " + e.get(i));
+                }
+                
+                System.out.println("\nESTADOS GERAIS: \n");
+
+                System.out.println("Numero de Eventos: " + p.get(0) + "\n"); 
+                System.out.println("Tempo Total: " + p.get(1) + "\n"); 
 
                 interpreter("SOLICITA", l, p, t, e, even, evenNm);
     }
@@ -225,8 +263,8 @@ public class Main{
 
     public static ArrayList PseudoGenerator(int perSeed, double a, double M, double c, ArrayList L, int seed){
 
-        if (perSeed > 999){
-            perSeed = 999;
+        if (perSeed > 100){
+            perSeed = 100;
         }
 
         double qseed = seed;
